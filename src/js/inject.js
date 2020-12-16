02035d5afb237d4dabacd00be807d80e75244652
kai <kaijuneer@gmail.com>
2020-12-16 13:11:46 +0000

    can't access GVL list

--- src/js/inject.js
+++ src/js/inject.js
@@ -3,7 +3,7 @@
  */
 
 import * as cmpstub from '@iabtcf/stub';
-//import {TCModel, TCString, GVL} from '@iabtcf/core';
+import {TCModel, TCString, GVL} from '@iabtcf/core';
 import {CmpApi} from '@iabtcf/cmpapi';
 
 console.log("HELLO FROM inject.js", document);
@@ -13,3 +13,24 @@ cmpstub();
 //constructor input arguments: cmpID, cmpVersion, serviceSpecific
 const cmpApi = new CmpApi(141, 4, true);
 cmpApi.update('');
+
+/*
+// unable to access the GVL
+GVL.baseUrl = "https://vendor-list.consensu.org/v2/vendor-list.json";
+
+// create a new TC string
+const tcModel = new TCModel(new GVL());
+
+// Some fields will not be populated until a GVL is loaded
+tcModel.gvl.readyPromise.then(() => {
+
+  tcModel.cmpId = 141;
+  tcModel.cmpVersion = 4;
+  tcModel.isServiceSpecific = true;
+
+  const encodedString = TCString.encode(tcModel);
+
+  console.log(encodedString); // TC string encoded begins with 'C'
+
+});*/
+
