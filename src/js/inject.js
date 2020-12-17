bca5fedbf7aa7cb1ed0f478a3fd1497fd94c4fec
kai <kaijuneer@gmail.com>
2020-12-17 11:10:22 +0000

    use local vendor list

--- src/js/inject.js
+++ src/js/inject.js
@@ -6,6 +6,8 @@ import * as cmpstub from '@iabtcf/stub';
 import {TCModel, TCString, GVL} from '@iabtcf/core';
 import {CmpApi} from '@iabtcf/cmpapi';
 
+const gvljson = require("./data/vendor-list.json")
+
 console.log("HELLO FROM inject.js", document);
 
 // set up __tcfpi stub
@@ -14,23 +16,15 @@ cmpstub();
 const cmpApi = new CmpApi(141, 4, true);
 cmpApi.update('');
 
-/*
-// unable to access the GVL
-GVL.baseUrl = "https://vendor-list.consensu.org/v2/vendor-list.json";
+// set up GVL vendor list
+const gvl = new GVL(gvljson);
 
 // create a new TC string
-const tcModel = new TCModel(new GVL());
+const tcModel = new TCModel(gvl);
+tcModel.cmpId = 141;
+tcModel.cmpVersion = 4;
+tcModel.isServiceSpecific = true;
+const encodedString = TCString.encode(tcModel);
 
-// Some fields will not be populated until a GVL is loaded
-tcModel.gvl.readyPromise.then(() => {
-
-  tcModel.cmpId = 141;
-  tcModel.cmpVersion = 4;
-  tcModel.isServiceSpecific = true;
-
-  const encodedString = TCString.encode(tcModel);
-
-  console.log(encodedString); // TC string encoded begins with 'C'
-
-});*/
-
+console.log(encodedString); // TC string encoded begins with 'C'
+cmpApi.update(encodedString, false);
