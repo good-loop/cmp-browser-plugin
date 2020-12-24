d81472cbfef9b744b2787b8c406a5e7d01ca9738
kai <kaijuneer@gmail.com>
2020-12-24 10:30:51 +0000

    basic functionality to capture user's preference

--- src/js/options.js
+++ src/js/options.js
@@ -1,1 +1,15 @@
-// ??
+function save_options() {
+    var marketingConsent = document.getElementById('marketing').checked;
+    chrome.storage.sync.set({
+      marketing: marketingConsent
+    }, function() {
+      // Update status to let user know options were saved.
+      var status = document.getElementById('status');
+      status.textContent = 'Preferences saved.';
+      setTimeout(function() {
+        status.textContent = '';
+      }, 1000);
+    });
+}
+
+document.getElementById('save').addEventListener('click',save_options);
