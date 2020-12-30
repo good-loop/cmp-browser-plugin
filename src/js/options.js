f70f6c009c37fdc983edcd4ac41cf2e0ebf1a927
kai <kaijuneer@gmail.com>
2020-12-30 11:43:34 +0000

    users able to check current consent settings

--- src/js/options.js
+++ src/js/options.js
@@ -5,11 +5,21 @@ function save_options() {
     }, function() {
       // Update status to let user know options were saved.
       var status = document.getElementById('status');
-      status.textContent = 'Preferences saved.';
+      status.textContent = 'Preferences saved: ' + marketingConsent;
       setTimeout(function() {
         status.textContent = '';
       }, 1000);
     });
 }
 
+function get_options() {
+    chrome.storage.sync.get(['marketing'], function(result) {
+      console.log(result);
+	    var marketingConsent = result.marketing;
+      var settings = document.getElementById('settings');
+      settings.textContent = 'Marketing consent: ' + marketingConsent;
+	  });
+}
+
 document.getElementById('save').addEventListener('click',save_options);
+document.getElementById('check').addEventListener('click',get_options);
