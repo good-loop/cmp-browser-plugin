b7165d9bf7a58c7f4e60b843bd957231d6e57e1a
kai <kaijuneer@gmail.com>
2021-01-06 13:09:08 +0000

    to take more purpose options from users

--- src/js/options.js
+++ src/js/options.js
@@ -1,11 +1,13 @@
 function save_options() {
-    var marketingConsent = document.getElementById('marketing').checked;
+    var consent1 = document.getElementById('1').checked;
+    var consent2 = document.getElementById('2').checked;
     chrome.storage.sync.set({
-      marketing: marketingConsent
+      purpose1: consent1,
+      purpose2: consent2
     }, function() {
       // Update status to let user know options were saved.
       var status = document.getElementById('status');
-      status.textContent = 'Preferences saved: ' + marketingConsent;
+      status.textContent = 'Preferences saved';
       setTimeout(function() {
         status.textContent = '';
       }, 1000);
@@ -13,10 +15,10 @@ function save_options() {
 }
 
 function get_options() {
-    chrome.storage.sync.get(['marketing'], function(result) {
-	    var marketingConsent = result.marketing;
+    chrome.storage.sync.get(['purpose1'], function(result) {
+	    var consent1 = result.purpose1;
       var settings = document.getElementById('settings');
-      settings.textContent = 'Marketing consent: ' + marketingConsent;
+      settings.textContent = 'Storage consent: ' + consent1;
 	  });
 }
 
