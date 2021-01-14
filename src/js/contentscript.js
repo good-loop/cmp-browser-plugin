dc716c5ee0ec030573ab226c4db48fa1fa2368a6
kai <kaijuneer@gmail.com>
2021-01-14 12:38:10 +0000

    remove redundant comments

--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -1,19 +1,18 @@
-
-import $ from 'jquery';
-import {TCModel, TCString, GVL} from '@iabtcf/core';
-import {addScript} from './base/utils/miscutils';
 // load the kvstore options manager
 import kvstore from './kvstore';
+import {TCModel, TCString, GVL} from '@iabtcf/core';
+// import {addScript} from './base/utils/miscutils';
+// import $ from 'jquery';
+// import _ from 'lodash';
 
 const gvljson = require("./data/vendor-list.json");
 
-// import _ from 'lodash';
 const LOGTAG = "GL-extension";
 
 console.log(LOGTAG, "Hello :)", window, document);
 console.log(LOGTAG, "CMP?", kvstore.get("cmp"));
 
-// // let uOptions = chrome.runtime.getURL('options.html'); doesnt work
+// let uOptions = chrome.runtime.getURL('options.html'); doesnt work
 // let uLogo = chrome.extension.getURL('/img/logo.64.png'); //chrome.runtime.getURL('img/logo.64.png');
 // let uLogo2 = chrome.extension.getURL('img/logo.64.png');
 
@@ -27,13 +26,14 @@ tcModel.isServiceSpecific = true;
 var encodedString = TCString.encode(tcModel);
 console.log("Encoded string in content script: " + encodedString);
 
+/*
 function injectScript(func) {
 	let actualCode = '(' + func + ')();';
 	let script = document.createElement('script');
 	script.textContent = actualCode;
 	(document.head||document.documentElement).appendChild(script);
 	script.remove();
-};
+}; */
 
 function injectTcfApi() {
 	// inject.js being injected as an extension
@@ -42,13 +42,6 @@ function injectTcfApi() {
 	(document.head||document.documentElement).appendChild(script);
 	script.remove();
 }
-
-// TODO CMP functions (as seen on a fandom.com page)
-// getConsentData undefined callback
-// ping 2 callback
-// getTCData 2 callback [91]
-// addEventListener 2 callback
-// removeEventListener 2 callback
 
 if ( ! kvstore.get("cmp")) {
 	console.warn("My CMP is OFF!");	
@@ -72,32 +65,4 @@ if ( ! kvstore.get("cmp")) {
 			});
 		}
 	}, false);
-	/*
-	injectScript(function() {
-		console.log("HELLO FROM THE PAGE :)", window, document);
-		let fn = function (...args) {
-			console.warn("My CMP", args);
-		};
-		
-		Object.defineProperty(window, "__tcfapi",
-			{
-				value: fn,
-				writable: false,
-				enumerable: false,
-				configurable: false
-			}
-		);
-
-		Object.defineProperty(window, "__cmp",
-			{
-				value: fn,
-				writable: false,
-				enumerable: false,
-				configurable: false
-			}
-		);
-
-		console.warn("Good-Loop CMP set: window.__cmp", window.__cmp, window.__tcfapi);
-	});
-	*/
 }
--- src/js/inject.js
+++ src/js/inject.js
@@ -2,11 +2,9 @@
  * This code initialise __tcfapi on websites with default rejection
  */
 import * as cmpstub from './lib/stub.js';
+import {CmpApi} from './lib/CmpApi.js';
 //import * as cmpstub from '@iabtcf/stub';
 //import {CmpApi} from '@iabtcf/cmpapi';
-import {CmpApi} from './lib/CmpApi.js';
-
-//const gvljson = require("./data/vendor-list.json");
 
 console.log("HELLO FROM inject.js", document);
 
@@ -15,6 +13,8 @@ cmpstub();
 //constructor input arguments: cmpID, cmpVersion, serviceSpecific
 const cmpApi = new CmpApi(141, 4, true);
 
+// obtain tcString from content script
+// MESSAGING CHANNEL ********************************
 window.postMessage({connection_setup:true}, '*');
 window.addEventListener("message", function(event) {
   if (event.data.connection_response) {
@@ -24,47 +24,7 @@ window.addEventListener("message", function(event) {
     cmpApi.update(encodedString, false);
   };
 }, false);
-
-
-/*
-// set up GVL vendor list
-const gvl = new GVL(gvljson);
-
-// create a new TC string
-const tcModel = new TCModel(gvl);
-tcModel.cmpId = 141;
-tcModel.cmpVersion = 4;
-tcModel.isServiceSpecific = true;
-
-const encodedString = TCString.encode(tcModel);
-
-console.log(encodedString); // TC string encoded begins with 'C'
-//CO_971SO_971SCNAEAENBECgAAAAAAAAAAAAAAAAAAAA.YAAAAAAAAAAA
-cmpApi.update(encodedString, false);
-
-// TODO: __tcfapi in some websites are not overridden, consent popup is still there
-// TODO: set tcModel based on user's preferences
-// MESSAGING CHANNEL ****************************
-window.postMessage({connection_setup:true}, '*');
-window.addEventListener("message", function(event) {
-  if (event.data.connection_response) {
-    console.log("connection successful!");
-    var i;
-    for (i=0; i<10; i++) {
-      if (event.data.purposes[i]) tcModel.purposeConsents.set(i+1);
-    }
-    const newEncodedString = TCString.encode(tcModel);
-    console.log(newEncodedString);
-    cmpApi.update(newEncodedString, false);
-  };
-}, false);
-/*
-var extensionId = "gpgihgkjcdkilmibcpffonlglmkmiehh";
-chrome.runtime.sendMessage(extensionId, {message: "I also need data"},
-  function(response) {
-    console.log("Collecting response from extension: " + response.marketing);
-});*/
-// *********************************************
+// **************************************************
 
 // purpose IDs and their meanings **************
 // 1: Store information on device
