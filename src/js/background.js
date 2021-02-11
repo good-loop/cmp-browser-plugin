361e80fc6a7df7fe12f27dedcf85bccd1ed51483
Eer Kai Jun <kaijuneer@gmail.com>
2021-02-11 11:49:04 +0000

    fetch data periodically

--- src/js/background.js
+++ src/js/background.js
@@ -18,13 +18,24 @@ chrome.runtime.onInstalled.addListener(function(details) {
         chrome.storage.sync.set({
             purposes: [false, false, false, false, false,
                         false, false, false, false, false]
-        })
+        });
+        updateList();
+        // create an alarm which goes off every one week to update data lists
+        chrome.alarms.create("Periodic update datalist", {periodInMinutes: 10080});
     }
 }); 
+
+chrome.alarms.onAlarm.addListener(function(alarm) {
+    if (alarm.name == "Periodic update datalist") {
+        console.log("Got an alarm!", alarm);
+        updateList();
+    }
+});
 
 async function updateList() {
     const response = await fetch('https://raw.githubusercontent.com/good-loop/cmp-browser-plugin/develop/src/js/data/vendor-list.json');
     const gvljson = await response.json();
+    chrome.storage.local.set({vendorlist: gvljson}, function(){
+        console.log("Done updating!");
+    });
 }
-
-updateList();
