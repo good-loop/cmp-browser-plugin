9f24fa482a503506c3232ecdfe2acd6dc4852a47
Eer Kai Jun <kaijuneer@gmail.com>
2021-02-24 09:54:24 +0000

    install page3

--- extension/install.js
+++ extension/install.js
@@ -1,7 +1,17 @@
 document.getElementById('next').addEventListener('click', nextpage);
 
 function nextpage() {
-    const url = chrome.extension.getURL('install-page2.html');
-    window.open(url, "_self");
+    var url;
+    html = location.pathname.split('/').slice(-1)[0]
+    if (html == "install-page1.html") {
+        url = chrome.extension.getURL("install-page2.html");
+        window.open(url, "_self");
+    } else if (html == "install-page2.html") {
+        url = chrome.extension.getURL("install-page3.html");
+        window.open(url, "_self");
+    } else if (html == "install-page3.html") { 
+        window.close() 
+    }
+    
 }
 
